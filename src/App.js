5215efd6fd826e8e4fa61a625d11e17797885103
VishalBhuse <vbhuse1997@gmail.com>
2022-06-14 10:37:02 +0530

    create component

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+
+import Navbar from "./Component/Navbar";
+import Home from "./Component/Home";
+import Products from "./Component/Products";
+import Product from "./Component/Product";
+import Footer from "./Component/Footer";
+import Login from "./Component/Login";
+import Dashboard from "./Component/Dashboard";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:id" element={<Product />} />
+      </Routes>
+
+      <Footer />
+    </>
+  );
+}
+
+export default App;
