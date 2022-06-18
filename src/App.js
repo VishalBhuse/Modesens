4e7dcaa85d91405e0b7a78d97864c99dd5a1cccf
bheem ratan <devendrakumar501425@gmail.com>
2022-06-18 08:49:21 +0530

    like page

--- src/App.js
+++ src/App.js
@@ -5,14 +5,12 @@ import Navbar from "./Component/Navbar";
 import Product from "./Component/Product";
 import Footer from "./Component/Footer";
 import Login from "./Component/Login";
+
 import Dashboard from "./Component/Dashboard";
-<<<<<<< HEAD
-import Likes from "./Component/Likes";
-=======
 import Products from "./Component/productspage/Products";
 import Homeindex from "./Component/Homeindex";
-// import { Filters } from "./Component/productspage/Filters";
->>>>>>> 68b7a48b7e52d2f31db8307ded3ea10cdf1236ca
+import Likes from "./Component/Likes"
+
 
 function App() {
   return (
